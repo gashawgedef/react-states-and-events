476c9dfa7a0bb555c30fb1c18c5ebb06fd2fd2c5
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-02 09:00:54 -0700

    gashaw

--- state-and-events/src/App.js
+++ state-and-events/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Events from './Events';
+import Example from './Example';
 
 function App() {
   return (
     <div className="App">
       <Events/>
+      <Example/>
     </div>
   );
 }
