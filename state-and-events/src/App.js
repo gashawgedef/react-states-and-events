29051370fd99af437d1a215cc391c719cefa0f42
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-02 10:25:01 -0700

    Gashaw gedef

--- state-and-events/src/App.js
+++ state-and-events/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Events from './Events';
 import Example from './Example';
+import Gashity from './Gashity';
 
 function App() {
   return (
     <div className="App">
       <Events/>
       <Example/>
+      <Gashity/>
     </div>
   );
 }
---
+++ state-and-events/src/Gashity.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gashity() {
+  return (
+    <div>
+        Hello Teams welcome to This page
+    </div>
+  )
+}
+
+export default Gashity
