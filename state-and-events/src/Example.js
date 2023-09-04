901f1a56e2e066291d5e064b1c8239611a1428c2
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-04 10:06:49 -0700

    commit message

--- state-and-events/src/Example.js
+++ state-and-events/src/Example.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 
+const message=[
+  "Learn for React *",
+  "Apply for jobs",
+  "Invest your new income"
+]
 export class Example extends Component {
+
   render() {
+    const step=1;
     return (
       <div className='steps'>
         <div className='numbers'>
-          <div>1</div>
+          <div className='active'>1</div>
           <div>2</div>
           <div>3</div>
         </div>
-        <p className='message'>Hello Message</p>
+        <p className='message'>Step {step}:{message[step-1]}</p>
         <div className='buttons'>
           <button style={{backgroundColor:'#7950f2',color:'#fff'}}>Previous</button>
           <button style={{backgroundColor:'#7950f2',color:'#fff'}}>Next</button>
