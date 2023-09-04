ac4b9bccde789bd333eada3510999d77d6baafd5
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-04 15:03:22 -0700

    gashity g

--- state-and-events/src/Gashity.js
+++ state-and-events/src/Gashity.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+const message = [
+  "Learn for React *",
+  "Apply for jobs",
+  "Invest your new income",
+];
 
 function Gashity() {
+
+  const [step,setStep]=useState(1)
+  function handlePrevious() {
+    alert("Previous");
+  }
+  function handleNext() {
+    setStep(step+1)
+  }
+
   return (
-    <div>
-        Hello Teams welcome to This page
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}:{message[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Gashity
+export default Gashity;
