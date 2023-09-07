d6c2c78a7dd6fb8ce323bef0966c472d3793165e
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-09-07 16:51:21 -0700

    my commit message

--- state-and-events/src/App.js
+++ state-and-events/src/App.js
@@ -4,12 +4,16 @@ import Events from './Events';
 import Example from './Example';
 import Gashity from './Gashity';
 import Counter from './Counter';
+import Api from './Api';
+import Template from './template';
 
 function App() {
   return (
     <div className="App">
       {/* <Events/>
       <Example/> */}
+      <Template/>
+      <Api/>
       <Counter/>
       <Gashity/>
     </div>
---
+++ state-and-events/src/Template.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Template extends Component {
+  render() {
+    return (
+      <div>
+        Hello This is front-end template
+        
+      </div>
+    )
+  }
+}
+
+export default Template
